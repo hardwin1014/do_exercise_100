acbfd047ff9ea4c5fe2e9843c0d33c7e12c3d6b6
codewxh <914067438@qq.com>
2022-04-13 20:27:57 +0800

    1

--- src/01-algorithm/queue-with-list.ts
+++ src/01-algorithm/queue-with-list.ts
@@ -13,14 +13,14 @@ export class MyQueue {
     // 定义头尾和长度
     private head: IListNode | null = null
     private tail: IListNode | null = null
-    private len = 0
+    private len = 0 // length单独存储
 
     /*
-    * 入队  在tail入队
+    * 入队  在tail入队 从head出队
     * @param n number
     * */
     add(n: number){
-        // 定义节点的模型
+        // 定义链表的节点的形式
         const newNode: IListNode = {
             value: n,
             next:null
@@ -44,6 +44,7 @@ export class MyQueue {
         this.len ++
     }
 
+    // head 出队, 正常删除是number，删除，没了就是null
     delete(): number | null{
         const headNode = this.tail
         if(headNode == null) return null
@@ -68,11 +69,35 @@ export class MyQueue {
     }
 }
 
-const q = new MyQueue()
-q.add(100)
-q.add(200)
-q.add(300)
-q.add(400)
-console.info(q.length)
-console.log(q.delete())
-console.log(q.length)
+// 功能测试
+
+// const q = new MyQueue()
+// q.add(100)
+// q.add(200)
+// q.add(300)
+// q.add(400)
+// console.info(q.length)
+// console.log(q.delete())
+// console.log(q.length)
+
+// 性能测试
+const q1 = new MyQueue()
+console.time('queue with list')
+for(let i = 0; i < 10*10000; i++){
+    q1.add(i)
+}
+for(let i = 0; i < 10*10000;i++){
+    q1.delete()
+}
+console.timeEnd('quque with list')
+
+const q2 = []
+
+console.time('queue with arr')
+for(let i = 0; i < 10*10000; i++){
+    q2.push(i)
+}
+for(let i = 0; i < 10*10000;i++){
+    q2.shift()
+}
+console.timeEnd('quque with arr')
