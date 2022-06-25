971d878511f156aa456032b0217e09af9c92db94
codewxh <914067438@qq.com>
2022-06-25 10:40:20 +0800

    hash table

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,17 @@
 // import './01-algorithm/palindrome-number'
 // import './01-algorithm/thousands-format'
 import './01-algorithm/switch-letter-case'
+
+
+/**
+ * @description 如何实现高效的英文单词前缀匹配
+ * 把全部的单词遍历成一个hash table逻辑结构
+ * Object物理结构
+ *
+ * 考虑优化原始数据结构
+ * 有明确范围的数据*（如26个英文字母）、考虑使用哈希表（对象）
+ *
+ * 如遍历数组,时间复杂度至少O(n)起步(n是数组长度)
+ * 而改为树,时间复杂度降低到O(m)（m是单词的长度）
+ * PS: 哈希表(对象)通过key查询，时间复杂度为O(1)
+*/
